fbb4daef39aeb2bdb878e91648e6d2633148c764
ananthan <ananthan@salesfokuz.in>
2023-08-02 12:09:41 +0530

    first commit

---
+++ lib/dynamic_form_fields.dart
@@ -0,0 +1,162 @@
+import 'dart:convert';
+
+import 'package:dynamic_form_lead/jsonfile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+class DynamicFormPage extends StatefulWidget {
+  final Map<String, dynamic> jsonData;
+
+  DynamicFormPage({required this.jsonData});
+
+  @override
+  _DynamicFormPageState createState() => _DynamicFormPageState();
+}
+
+class _DynamicFormPageState extends State<DynamicFormPage> {
+  final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
+
+  String form =
+  json.encode({
+    "fields": [
+      {
+        "key": "first_name",
+        "type": "Input",
+        "label": "First Name",
+        "validation": "required"
+      },
+      {
+        "key": "last_name",
+        "type": "Input",
+        "label": "Last Name",
+        "validation": ""
+      },
+      {
+        "key": "email",
+        "type": "Input",
+        "label": "Email",
+        "validation": "required"
+      },
+      {
+        "label": "Phone",
+        "key": "phone",
+        "type": "Input",
+        "validation": "required"
+      },
+      {"label": "Age", "key": "age", "type": "Input", "validation": "required"},
+
+      {
+        "type": "Select",
+        "key": "country",
+        "label": "Country",
+        'items': [
+          {
+            'label': "product 1",
+            'value': "product 1",
+          },
+          {
+            'label': "product 2",
+            'value': "product 2",
+          },
+          {
+            'label': "product 3",
+            'value': "product 3",
+          }
+        ]
+      }
+    ],
+    "table_name": "data_collection",
+    "response_code": 200,
+    "response_string": "success"
+  });
+  bool _progressBarActive = true;
+
+  @override
+  void initState() {
+
+    _progressBarActive = false;
+    setState(() {
+
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Dynamic Form')),
+      body: getBody(),
+    );
+  }
+
+
+  Widget getBody() {
+    return _progressBarActive != true
+        ? form != ""
+        ?  SingleChildScrollView(
+      child:  Container(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+          child:  Column(children: <Widget>[
+             JsonSchema(
+              form: form,
+              onChanged: (dynamic response) {
+                // this.response = response;
+                print(response);
+              },
+              actionSave: (data) {
+                print(data);
+                print(data['fields']);
+                // upload(data);
+                // datas = data;
+              },
+              buttonSave:
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  margin: EdgeInsets.only(
+                    top: 5,
+                    bottom: 15,
+                  ),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: InkWell(
+                      // onTap: () {
+                      //   if (submitButtonClickEnable) {
+                      //     submitValidate();
+                      //   }
+                      // },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color:Colors.green,
+                        ),
+                        width: 100,
+                        height: 40,
+                        child: Center(
+                          child: Text(
+                            "SUBMIT",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 10,
+                              fontFamily: "UbuntuRegular",
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ])),
+    )
+        : Center(
+      child: Text("NO DATA"),
+    )
+        : Center(
+      child: const CircularProgressIndicator(),
+    );
+  }
+
+}
---
+++ lib/jsonfile.dart
@@ -0,0 +1,407 @@
+
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+class JsonSchema extends StatefulWidget {
+  const JsonSchema({
+    required this.form,
+    required this.onChanged,
+    this.padding,
+    this.formMap,
+    this.errorMessages = const {},
+    this.validations = const {},
+    this.decorations = const {},
+    this.buttonSave,
+    this.actionSave,
+  });
+
+  final Map errorMessages;
+  final Map validations;
+  final Map decorations;
+  final String form;
+  final Map? formMap;
+  final double? padding;
+  final Widget? buttonSave;
+  final Function? actionSave;
+  final ValueChanged<dynamic> onChanged;
+
+  @override
+  _CoreFormState createState() =>
+       _CoreFormState(formMap ?? json.decode(form));
+}
+
+class _CoreFormState extends State<JsonSchema> {
+  final dynamic formGeneral;
+
+  int? radioValue;
+
+  // validators
+
+  String isRequired(item, value) {
+    if (value.isEmpty) {
+      return widget.errorMessages[item['key']] ?? 'This field is required!';
+    }
+    return '';
+  }
+
+  String validateEmail(item, String value) {
+    String p = "[a-zA-Z0-9\+\.\_\%\-\+]{1,256}" +
+        "\\@" +
+        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+        "(" +
+        "\\." +
+        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+        ")+";
+    RegExp regExp =  RegExp(p);
+
+    if (regExp.hasMatch(value)) {
+      return '';
+    }
+    return 'Email is not valid';
+  }
+
+  String validatePhone(item, String value) {
+    if (value.length != 10)
+      return 'Mobile Number must be of 10 digit';
+    else
+      return '';
+  }
+
+  bool labelHidden(item) {
+    if (item.containsKey('hiddenLabel')) {
+      if (item['hiddenLabel'] is bool) {
+        return !item['hiddenLabel'];
+      }
+    } else {
+      return true;
+    }
+    return false;
+  }
+
+  // Return widgets
+
+  List<Widget> jsonToForm() {
+    List<Widget> listWidget = [];
+    if (formGeneral['title'] != null) {
+      listWidget.add(Text(
+        formGeneral['title'],
+        style:  const TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+      ));
+    }
+    if (formGeneral['description'] != null) {
+      listWidget.add(Text(
+        formGeneral['description'],
+        style:  const TextStyle(fontSize: 14.0, fontStyle: FontStyle.italic),
+      ));
+    }
+
+    for (var count = 0; count < formGeneral['fields'].length; count++) {
+      Map item = formGeneral['fields'][count];
+
+      if (item['type'] == "Phone" ||
+          item['type'] == "Password" ||
+          item['type'] == "Email" ||
+          item['type'] == "TextArea" ||
+          item['type'] == "Input") {
+        Widget label = const SizedBox.shrink();
+        if (labelHidden(item)) {
+          label =  Container(
+            child:  Text(
+              item['label'],
+              style: const TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'UbuntuRegular',
+                  color: Colors.grey),
+            ),
+          );
+        }
+
+        listWidget.add( Container(
+          margin:  const EdgeInsets.only(top: 10.0),
+          child: ListTile(
+            // leading: Container(
+            //   width: .5,
+            // ),
+            title: Container(
+              // padding: EdgeInsets.only(
+                  // left: SizeConfig.safeBlockVertical * 1.5
+                  // top: SizeConfig.safeBlockVertical * 1.5
+                  // ),
+              child: label,
+            ),
+            subtitle: Container(
+              margin: const EdgeInsets.only(
+                  // left: SizeConfig.safeBlockVertical * 1.5,
+                  right:5,
+                  bottom: 5),
+              child:  TextFormField(
+                textInputAction: item['type'] == "TextArea"?null:TextInputAction.next,
+                textAlign: TextAlign.start,
+                textCapitalization: TextCapitalization.words,
+                controller: null,
+                initialValue: formGeneral['fields'][count]['value'],
+                decoration: item['decoration'] ??
+                    widget.decorations[item['key']] ??
+                     InputDecoration(
+                      // hintText: item['placeholder'] ?? "",
+                      // helperText: item['helpText'] ?? "",
+                      contentPadding: const EdgeInsets.only(
+                        top: 2,
+                        bottom: 2,
+                      ),
+                      isDense: true,
+                      hintText: 'Enter ' + item['label'],
+                      hintStyle: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          fontFamily: 'UbuntuRegular'),
+                      border: const UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.black,
+                          width: 1.0,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                    ),
+                maxLines: item['type'] == "TextArea" ? 5 : 1,
+                // keyboardType: item['type'] == "Phone" ? TextInputType.phone : TextInputType.text,
+                keyboardType: item['type'] == "Phone"
+                    ? TextInputType.phone
+                    : item['type'] == "TextArea"
+                        ? TextInputType.multiline
+                        : TextInputType.text,
+                onChanged: (String value) {
+                  formGeneral['fields'][count]['value'] = value;
+                  _handleChanged();
+                },
+                obscureText: item['type'] == "Password" ? true : false,
+                validator: (value) {
+                  if (widget.validations.containsKey(item['key'])) {
+                    return widget.validations[item['key']](item, value);
+                  }
+                  if (item.containsKey('validator')) {
+                    if (item['validator'] != null) {
+                      if (item['validator'] is Function) {
+                        return item['validator'](item, value);
+                      }
+                    }
+                  }
+
+                  if (item['type'] == "Email") {
+                    if (item['required'] == true ||
+                        item['required'] == 'True' ||
+                        item['required'] == 'true') {
+                      return validateEmail(item, value!);
+                    }
+                  }
+                  if (item['type'] == "Phone") {
+                    if (item['required'] == true ||
+                        item['required'] == 'True' ||
+                        item['required'] == 'true') {
+                      return validatePhone(item, value!);
+                    }
+                  }
+
+                  if (item.containsKey('required')) {
+                    if (item['required'] == true ||
+                        item['required'] == 'True' ||
+                        item['required'] == 'true') {
+                      return isRequired(item, value);
+                    }
+                  }
+
+                  return null;
+                },
+              ),
+            ),
+          ),
+        ));
+      }
+
+      if (item['type'] == "RadioButton") {
+        List<Widget> radios = [];
+
+        if (labelHidden(item)) {
+          radios.add( Text(item['label'],
+              style:
+                   const TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0)));
+        }
+        radioValue = item['value'];
+        for (var i = 0; i < item['items'].length; i++) {
+          radios.add(
+             Row(
+              children: <Widget>[
+                 Expanded(
+                    child:  Text(
+                        formGeneral['fields'][count]['items'][i]['label'])),
+                 Radio<int>(
+                    value: formGeneral['fields'][count]['items'][i]['value'],
+                    groupValue: radioValue,
+                    onChanged: (int? value) {
+                      setState(() {
+                        radioValue = value!;
+                        formGeneral['fields'][count]['value'] = value;
+                        _handleChanged();
+                      });
+                    })
+              ],
+            ),
+          );
+        }
+
+        listWidget.add(
+           Container(
+            margin:  const EdgeInsets.only(top: 5.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: radios,
+            ),
+          ),
+        );
+      }
+
+      if (item['type'] == "Switch") {
+        if (item['value'] == null) {
+          formGeneral['fields'][count]['value'] = false;
+        }
+        listWidget.add(
+           Container(
+            margin:  const EdgeInsets.only(top: 5.0),
+            child:  Row(children: <Widget>[
+               Expanded(child:  Text(item['label'])),
+               Switch(
+                value: item['value'] ?? false,
+                onChanged: (bool value) {
+                  setState(() {
+                    formGeneral['fields'][count]['value'] = value;
+                    _handleChanged();
+                  });
+                },
+              ),
+            ]),
+          ),
+        );
+      }
+
+      if (item['type'] == "Checkbox") {
+        List<Widget> checkboxes = [];
+        if (labelHidden(item)) {
+          checkboxes.add( Text(item['label'],
+              style:
+                   const TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0)));
+        }
+        for (var i = 0; i < item['items'].length; i++) {
+          checkboxes.add(
+             Row(
+              children: <Widget>[
+                 Expanded(
+                    child:  Text(
+                        formGeneral['fields'][count]['items'][i]['label'])),
+                 Checkbox(
+                  value: formGeneral['fields'][count]['items'][i]['value'],
+                  onChanged: (bool? value) {
+                    setState(
+                      () {
+                        formGeneral['fields'][count]['items'][i]['value'] =
+                            value;
+                        _handleChanged();
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          );
+        }
+
+        listWidget.add(
+           Container(
+            margin:  const EdgeInsets.only(top: 5.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: checkboxes,
+            ),
+          ),
+        );
+      }
+
+      if (item['type'] == "Select") {
+        Widget label = const SizedBox.shrink();
+        if (labelHidden(item)) {
+          label =  Text(item['label'],
+              style:
+                   const TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0));
+        }
+
+        listWidget.add( Container(
+          margin:  const EdgeInsets.only(top: 5.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              label,
+               DropdownButton<String>(
+                hint:  const Text("Select a user"),
+                value: formGeneral['fields'][count]['value'],
+                onChanged: (String? Value) {
+                  setState(() {
+                    formGeneral['fields'][count]['value'] = Value;
+                    _handleChanged();
+                  });
+                },
+                items:
+                    item['items'].map<DropdownMenuItem<String>>((dynamic data) {
+                  return DropdownMenuItem<String>(
+                    value: data['value'],
+                    child:  Text(
+                      data['label'],
+                      style:  const TextStyle(color: Colors.black),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+        ));
+      }
+    }
+
+    if (widget.buttonSave != null) {
+      listWidget.add( Container(
+        margin: const EdgeInsets.only(top: 10.0),
+        child: InkWell(
+          onTap: () {
+            if (_formKey.currentState!.validate()) {
+              widget.actionSave!(formGeneral);
+            }
+          },
+          child: widget.buttonSave,
+        ),
+      ));
+    }
+    return listWidget;
+  }
+
+  _CoreFormState(this.formGeneral);
+
+  void _handleChanged() {
+    widget.onChanged(formGeneral);
+  }
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Form(
+      // autovalidateMode: formGeneral['autoValidated'] ?? false,
+      key: _formKey,
+      child:  Container(
+        padding:  EdgeInsets.all(widget.padding ?? 8.0),
+        child:  Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: jsonToForm(),
+        ),
+      ),
+    );
+  }
+}
